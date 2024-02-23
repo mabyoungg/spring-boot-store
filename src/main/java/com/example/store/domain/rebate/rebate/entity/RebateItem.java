d2ba3a25914a3b3ae2490087d52e54c93f2e5c9d
mabyoungg <mabyoungg@gmail.com>
2024-02-23 22:38:09 +0900

    refactor: RebateItem entity 외래키 제약조건 제거

--- src/main/java/com/example/store/domain/rebate/rebate/entity/RebateItem.java
+++ src/main/java/com/example/store/domain/rebate/rebate/entity/RebateItem.java
@@ -4,9 +4,7 @@ import com.example.store.domain.member.member.entity.Member;
 import com.example.store.domain.product.order.entity.OrderItem;
 import com.example.store.domain.product.product.entity.Product;
 import com.example.store.global.jpa.BaseTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -24,12 +22,16 @@ public class RebateItem extends BaseTime {
     private LocalDateTime eventDate; // 판매(구매)가 발생한 날짜
     private LocalDateTime rebateDate; // 정산일
     @OneToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private OrderItem orderItem; // 주문상품
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Member seller; // 판매자
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Member buyer; // 구매자
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Product product; // 상품
     private long payPrice; // 결제금액
     private double rebateRate; // 정산율
